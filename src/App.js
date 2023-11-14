fa4427f263a63d8ca7bfae4af91e1b988002ec9a
Nika <ccnico8@gmail.com>
2023-11-14 16:52:33 -0500

    v01 modifique app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      BIENVENIDOS
+      <h3>Instituto de Educación Superior Honorio Delgado Espinoza</h3>
+      <h5>Nicole Sara Cabrera Camargo</h5>
+      TURNO: NOCHE
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
